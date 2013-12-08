b73a2de3cc1c8b60269cd6034fbcb682f1f07f41
CareBear Kate <carebearkasia@gmail.com>
2013-12-08 16:50:36 -0600

    [Kate/David] Add angular to existant view

---
+++ CSharpWarrior/Scripts/app/app.js
@@ -0,0 +1,7 @@
+﻿angular
+    .module('warrior', [])
+    .controller('warriorController', ['$scope', function ($scope) {
+
+        $scope.name = 'Warrior, the angular is strong with you, but you must complete with your training.';
+
+    }]);
