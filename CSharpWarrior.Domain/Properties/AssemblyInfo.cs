4f6e59f5c7ff02f5387e840d90437c90620db4c1
CareBear Kate <carebearkasia@gmail.com>
2013-12-13 16:22:04 -0600

    [David/Kate] Add domain project

---
+++ CSharpWarrior.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharpWarrior.Domain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("CSharpWarrior")]
+
+[assembly: ComVisible(false)]
