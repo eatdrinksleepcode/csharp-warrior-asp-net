dbed4909ebb1f00d96ad87821fe630898db0cbd0
eatdrinksleepcode <danelson@NA7761.corporate.thoughtworks.com>
2014-01-10 20:56:06 -0500

    [David] Execute user code

--- CSharpWarrior.Web/Scripts/app/app.js
+++ CSharpWarrior.Web/Scripts/app/app.js
@@ -1,7 +1,14 @@
 ﻿angular
     .module('warrior', [])
-    .controller('warriorController', ['$scope', function ($scope) {
+    .controller('warriorController', ['$scope', '$http', function ($scope, $http) {
 
         $scope.name = 'Warrior, the angular is strong with you, but you must complete with your training.';
+        $scope.code = "public class Player { }";
 
+        $scope.run = function () {
+            $http.post('/api/level/1', { code: $scope.code })
+            .success(function (data) {
+                $scope.output = data;
+            });
+        };
     }]);
